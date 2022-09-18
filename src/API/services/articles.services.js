90044726ee502355cb9e281a8cb081bff1d1dc9d
osama-rashid-alahmed <osamarashidalahmed@gmail.com>
2022-09-18 23:11:18 +0300

    Improve ArticleService by adding extra functions

--- src/API/services/articles.services.js
+++ src/API/services/articles.services.js
@@ -1,23 +1,47 @@
 import { api } from "../Intercepter"
 
 class ArticlesService {
+  async create(title, postId, section){
+    try{
+      return api.post('/article/create', {title, post_id: postId, section});
+    }catch(error){
+      return error
+    }
+  }
   async getAll() {
     try{
-      const articles = await api.get("article/");
-      return articles.data.data;
+      const articles = await api.get("/article");
+      return articles.data;
     }catch(error){
       return error;
     }
   }
   async getOne(id) {
     try {
-      const article = await api.get(`/article/${id}`);
-      return article.data.data
+      const article = await api.post(`/article/show`, {article_id: id});
+      return article.data
     }catch(error){
       return error;
     }
-
   }
+
+  async deleteById(id) {
+    try {
+      return await api.post(`/article/delete`, {article_id: id});
+    }catch(error){
+      return error;
+    }
+  }
+
+  async getArticlesByUserId(id) {
+    try {
+      const articles = await api.post(`/article/articles-by-user`, {user_id: id});
+      return articles.data;
+    }catch(error){
+      return error;
+    }
+  }
+  
   updateArticle(id) {
     return true;
   }
