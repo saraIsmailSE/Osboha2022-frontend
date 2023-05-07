4392dcd295199484316e393493946321183e797d
saraIsmailSE <saraismailse@gmail.com>
2023-05-07 17:16:30 +0300

    --

--- src/views/Apps/Osboha/OsbohaApp.vue
+++ src/views/Apps/Osboha/OsbohaApp.vue
@@ -1,72 +1,38 @@
 <template>
   <div class="row">
-    <div class="col-lg-8 row m-0 p-0">
+
+    <!-- story starts -->
+    <div :style="`background-image: url(${knowAboutOsboha});`" class="story">
+    </div>
+    <!-- story ends -->
+
+    <!-- story starts -->
+    <div :style="`background-image: url(${latestBook});`" class="story" v-if="latest_book">
+      <h5>
+        {{ latest_book.name }}
+
+        <br>
+        <small class="h6 badge bg-info">{{ latest_book.section.section }}</small>
+
+      </h5>
+    </div>
+    <!-- story ends -->
+
+    <div class="col-12 row m-0 p-0 mt-2">
       <div class="col-sm-12">
         <tab-content id="pills-tabContent-2">
-          <tab-content-item
-            :active="true"
-            id="profile-feed"
-            aria-labelled-by="pills-feed-tab"
-          >
+          <tab-content-item :active="true" id="profile-feed" aria-labelled-by="pills-feed-tab">
             <!-- ##### <AddPost> ##### -->
-            <AddPost
-              @add-post="addPost"
-              type="normal"
-              :timeline_id="timeline_id"
-            />
+            <AddPost @add-post="addPost" type="normal" :timeline_id="timeline_id" />
             <!-- ##### <AddPost /> ##### -->
           </tab-content-item>
         </tab-content>
       </div>
       <tab-content id="pills-tabContent-2">
-        <tab-content-item
-          :active="true"
-          id="profile-feed"
-          aria-labelled-by="pills-feed-tab"
-        >
+        <tab-content-item :active="true" id="profile-feed" aria-labelled-by="pills-feed-tab">
           <LazyLoadedPosts :isMain="true" ref="lazyLoadedPostsRef" />
         </tab-content-item>
       </tab-content>
-    </div>
-    <div class="col-lg-4" v-show="!isMobile">
-      <!-- ##### Books ##### -->
-      <iq-card>
-        <template v-slot:headerTitle>
-          <h4 class="card-title">أخر الكتب المضافة</h4>
-        </template>
-        <template v-slot:body>
-          <ul class="media-story m-0 p-0">
-            <li
-              v-for="(book, index) in books"
-              :key="index"
-              class="d-flex align-items-center active"
-            >
-              <img
-                src="@/assets/images/main/book.png"
-                alt="story-img"
-                class="rounded-circle img-fluid"
-              />
-              <div class="stories-data ms-3">
-                <router-link
-                  :to="{
-                    name: 'book.book-details',
-                    params: { book_id: book.id },
-                  }"
-                >
-                  <h5 class="font-weight-bold">{{ book.name }}</h5>
-                </router-link>
-                <p class="mb-0">{{ book.section.section }}</p>
-              </div>
-            </li>
-          </ul>
-          <router-link :to="{ name: 'osboha.book' }">
-            <button class="btn btn-primary d-block mt-3 w-100">
-              مشاهدة الكل
-            </button>
-          </router-link>
-        </template>
-      </iq-card>
-      <!-- ##### End Books ##### -->
     </div>
   </div>
 </template>
@@ -74,26 +40,21 @@
 import AddPost from "@/components/post/add/AddPost";
 import LazyLoadedPosts from "@/components/post/LazyLoadedPosts.vue";
 import bookService from "@/API/services/book.service";
+import knowAboutOsboha from '@/assets/images/main/know-about-osboha-2.png'
+import latestBook from '@/assets/images/main/latest-book.png'
 
 export default {
   name: "Osboha App",
   components: { AddPost, LazyLoadedPosts },
   data() {
     return {
-      books: [],
+      latest_book: null,
+      knowAboutOsboha,
+      latestBook
     };
   },
   async created() {
-    if (!this.isMobile) {
-      this.books = await bookService.latestBooks();
-    }
-  },
-  watch: {
-    async isMobile(newVal, oldVal) {
-      if (!newVal) {
-        this.books = await bookService.latestBooks();
-      }
-    },
+    this.latest_book = await bookService.latestBooks();
   },
   methods: {
     addPost(post) {
@@ -101,12 +62,35 @@ export default {
     },
   },
   computed: {
-    isMobile() {
-      return this.$vuetify.display.mobile;
-    },
     timeline_id() {
       return this.$store.getters.getUserProfile.timeline_id;
     },
   },
 };
 </script>
+
+<style scoped>
+/* story */
+.story {
+  position: relative;
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 120px;
+  height: 200px;
+  box-shadow: 0px 5px 17px -7px rgba(0, 0, 0, 0.75);
+  border-radius: 10px;
+  margin-right: 10px;
+  transition: transform 100ms ease-in;
+  cursor: pointer;
+}
+
+.story:hover {
+  transform: scale(1.07);
+}
+
+.story h5 {
+  position: absolute;
+  top: 35%;
+  color: #192e35;
+}
+</style>
