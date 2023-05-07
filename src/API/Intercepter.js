a30aa7d55a1da13a4dab06b9ead771b02d85e892
saraIsmailSE <saraismailse@gmail.com>
2023-05-07 17:31:11 +0300

    --

--- src/API/Intercepter.js
+++ src/API/Intercepter.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import router from "@/router";
 
-const baseURL = "http://127.0.0.1:8000/api/v1";
+const baseURL = "https://www.platform.osboha180.com/api/api/v1";
 const api = axios.create({
   baseURL: baseURL,
 });
