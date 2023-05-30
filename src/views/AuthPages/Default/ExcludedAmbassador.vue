52196fcb16e407e8d8eff0cac3743d4749cbe69e
saraIsmailSE <saraismailse@gmail.com>
2023-05-30 21:12:36 +0300

    excluded members

---
+++ src/views/AuthPages/Default/ExcludedAmbassador.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container-fluid p-0">
+        <div class="row no-gutters">
+            <div class="col-sm-12 text-center">
+                <div class="iq-error position-relative mt-5">
+                    <img src="@/assets/images/main/excluded-ambassador.png" class="img-fluid iq-error-img" alt="403" />
+                    <div>
+                        <h2 class="mb-2 text-center">تم استبعادك</h2>
+                        <h5 class="text-center m-auto w-75">
+                            بسبب عدم التزامك بالقراءة طيلة الأسابيع الماضية 
+                        </h5>
+                        <a href="javascript:void(0);" class="d-flex align-items-center m-3" @click="logout">
+                            <i class="material-symbols-outlined">logout</i>
+                            <span class="mobile-text d-lg-none ms-3">تسجيل خروج</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "NotAmbassadorInAnyGroup",
+    data() {
+        return {};
+    },
+    methods: {
+        logout() {
+            this.$store.dispatch("logout");
+        },
+    },
+};
+</script>
+  
